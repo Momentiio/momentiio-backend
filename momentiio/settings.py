d439e7fd321bed4ce3b53dd490c3fcccc958a4c2
sawyerb22 <sawyerbateman@Sawyers-MacBook-Pro.local>
2019-09-18 02:31:13 -0600

    create user, photo, likes ect, set up repo to handle image management

--- momentiio/settings.py
+++ momentiio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imagekit',
+    'social'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'social.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
---
+++ social/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from . import models
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Admin for photos."""
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """Custom user admin."""
+
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    """Admin for Likes."""
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin for Comments."""
