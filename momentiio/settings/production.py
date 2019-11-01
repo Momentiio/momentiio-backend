b98e9434a3e6b623ef4994d8ab2bb46ed3dba8b2
sawyerb22 <sawyerbateman@Sawyers-MacBook-Pro.local>
2019-11-01 16:13:34 -0600

    housekeeping

--- momentiio/settings/production.py
+++ momentiio/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = True
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(
-    default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    default=DATABASE_URL, conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
 DATABASES['default'] = dj_database_url.config()
@@ -14,9 +14,8 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'api-momentiio.herokuapp.com']
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-DEBUG = True
 TEMPLATE_DEBUG = True
